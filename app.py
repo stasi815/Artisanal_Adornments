0b4ccdf16a1dcfc846c1c5774a517101945e579d
stasi815 <anastasia.gallardo@students.makeschool.com>
2019-10-08 10:47:32 -0700

    added MongoDB and Item model

--- app.py
+++ app.py
@@ -1,21 +1,26 @@
 from flask import Flask, render_template
+from pymongo import MongoClient
+
+client = MongoClient()
+db = client.Playlister
+playlists = db.playlists
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     """Return homepage."""
-    return render_template('home.html', msg='Flask is Cool!!')
+    return render_template('home.html', msg='Welcome to the shop!!')
 
-items = [
-    { 'title': 'Yawanawa Earrings', 'description': 'Blue and Green beads' },
-    { 'title': 'Yawanawa Necklace', 'description': '6 inches long, red, yellow, black' }
-]
+# items = [
+#     { 'title': 'Yawanawa Earrings', 'description': 'Blue and Green beads' },
+#     { 'title': 'Yawanawa Necklace', 'description': '6 inches long, red, yellow, black' }
+# ]
 
 @app.route('/items')
 def playlists_index():
     """Show all items."""
-    return render_template('items_index.html', items=items)
+    return render_template('items_index.html', items=items.find())
 
 if __name__ == '__main__':
     app.run(debug=True)
