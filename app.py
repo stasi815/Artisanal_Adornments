b0a0958f01918ec4f40e0b895c4ebfa5027c6c5b
stasi815 <anastasia.gallardo@students.makeschool.com>
2019-10-08 10:25:43 -0700

    items_index working

--- app.py
+++ app.py
@@ -7,5 +7,15 @@ def index():
     """Return homepage."""
     return render_template('home.html', msg='Flask is Cool!!')
 
+items = [
+    { 'title': 'Yawanawa Earrings', 'description': 'Blue and Green beads' },
+    { 'title': 'Yawanawa Necklace', 'description': '6 inches long, red, yellow, black' }
+]
+
+@app.route('/items')
+def playlists_index():
+    """Show all items."""
+    return render_template('items_index.html', items=items)
+
 if __name__ == '__main__':
     app.run(debug=True)
