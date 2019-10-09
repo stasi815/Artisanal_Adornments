fbe9e31fc482611eeb34d8fb90a148329c65a7c4
stasi815 <anastasia.gallardo@students.makeschool.com>
2019-10-08 21:14:46 -0700

    Shoppers can see single shopping lists

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 from flask import Flask, render_template, request, redirect, url_for
 from pymongo import MongoClient
+from bson.objectid import ObjectId
+
 
 client = MongoClient()
 db = client.Artisanal_Adornments
@@ -35,9 +37,14 @@ def shopping_lists_submit():
         'description': request.form.get('description'),
         'images': request.form.get('images').split()
     }
-    shopping_lists.insert_one(shopping_list)
-    print(request.form.to_dict())
-    return redirect(url_for('shopping_lists_index'))
+    shopping_list_id = shopping_lists.insert_one(shopping_list).inserted_id
+    return redirect(url_for('shopping_lists_show', shopping_list_id=shopping_list_id))
+
+@app.route('/shopping_lists/<shopping_list_id>')
+def shopping_lists_show(shopping_list_id):
+    """Show a single shopping list."""
+    shopping_list = shopping_lists.find_one({'_id': ObjectId(shopping_list_id)})
+    return render_template('shopping_lists_show.html', shopping_list=shopping_list)
 
 if __name__ == '__main__':
     app.run(debug=True)
