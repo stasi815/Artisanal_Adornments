9c56954a616e7958c402cd95a92e04f56a088864
stasi815 <anastasia.gallardo@students.makeschool.com>
2019-10-08 21:40:37 -0700

    Shoppers can create, edit and update a shopping list

--- app.py
+++ app.py
@@ -27,7 +27,7 @@ def shopping_lists_index():
 @app.route('/shopping_lists/new')
 def shopping_lists_new():
     """Create a new Shopping List."""
-    return render_template('shopping_lists_new.html')
+    return render_template('shopping_lists_new.html', shopping_list={}, title='New Shopping List')
 
 @app.route('/shopping_lists', methods=['POST'])
 def shopping_lists_submit():
@@ -45,6 +45,25 @@ def shopping_lists_show(shopping_list_id):
     """Show a single shopping list."""
     shopping_list = shopping_lists.find_one({'_id': ObjectId(shopping_list_id)})
     return render_template('shopping_lists_show.html', shopping_list=shopping_list)
+
+@app.route('/shopping_lists/<shopping_list_id>/edit')
+def shopping_lists_edit(shopping_list_id):
+    """Show the edit form for a playlist."""
+    shopping_list = shopping_lists.find_one({'_id': ObjectId(shopping_list_id)})
+    return render_template('shopping_lists_edit.html', shopping_list=shopping_list, title='Edit Shopping List')
+
+@app.route('/shopping_lists/<shopping_list_id>/edit')
+def shopping_lists_update(shopping_list_id):
+    """Submit an edited playlist."""
+    updated_shopping_list = {
+        'title': request.form.get('title'),
+        'description': request.form.get('description'),
+        'images': request.form.get('images').split()
+    }
+    shopping_lists.update_one(
+        {'_id': ObjectId(shopping_list_id)},
+        {'$set': updated_shopping_list})
+    return redirect(url_for('shopping_lists_show', shopping_list_id=shopping_list_id))
 
 if __name__ == '__main__':
     app.run(debug=True)
