77a748b5b479b18183720e0a7f69870e70cd1078
stasi815 <anastasia.gallardo@students.makeschool.com>
2019-10-10 07:30:11 -0700

    added mLbas

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
 from flask import Flask, render_template, request, redirect, url_for
 from pymongo import MongoClient
 from bson.objectid import ObjectId
+import os
 
-
-client = MongoClient()
-db = client.Artisanal_Adornments
+host = os.environ.get('MONGODB_URI', 'mongodb://localhost:27017/Artisanal_Adornments')
+client = MongoClient(host=f'{host}?retryWrites=false')
+db = client.get_default_database()
 shopping_lists = db.shopping_lists
 
 app = Flask(__name__)
